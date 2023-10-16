fc78d763aa2c686a854b8141a6988c9bbda890c3
ShakeDev00 <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2023-10-16 16:51:44 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,69 +1,89 @@
 let personagem = document.querySelector("#personagem");
 
+let moeda = document.querySelector("#moeda");
+
 let topo = 0;
 let esquerdo = 0;
 
+let moedaX = parseInt( Math.random() * 620 );
+let moedaY = parseInt( Math.random() * 428 );
 
-document.addEventListener("keydown",teclapressionada);
+moeda.style.top = moedaY + "px";
+moeda.style.left = moedaX + "px";
 
-function teclapressionada(evento){
+
+
+document.addEventListener("keydown", teclapressionada);
+
+function teclapressionada(evento) {
     let tecla = evento.keyCode; /*Recupera o código da tecla que foi pressionada */
+    /*codigo da tecla*/ 
+    personagem.classList = "";
+    personagem.classList.add("andando");
 
-    if(tecla == 40){
+    if (tecla == 40) {
         topo += 10; /*topo = topo + 10 */
-        if(topo >= 410){
+        if (topo >= 410) {
             topo = 410;
         }
         personagem.style.top = topo + "px";
     }
-    else if(tecla == 38){
+    else if (tecla == 38) {
         topo -= 10; /*topo = topo - 10 */
-        if(topo <= 0){
+        if (topo <= 0) {
             topo = 0;
         }
         personagem.style.top = topo + "px";
+        personagem.classList.add("cima");
     }
-    else if(tecla == 39){
+    else if (tecla == 39) {
         esquerdo += 10; /*esquerdo = esquerdo + 10 */
-        personagem.style.left = esquerdo + "px";
-        if(esquerdo >= 598){
+        
+        if (esquerdo >= 598) {
             esquerdo = 598;
         }
-    }
-    else if(tecla == 37){
-        esquerdo -= 10; /*esquerdo = esquerdo - 10 */
         personagem.style.left = esquerdo + "px";
-        if(esquerdo <= 10){
+        personagem.classList.add("direita");
+    }
+    else if (tecla == 37) {
+        esquerdo -= 10; /*esquerdo = esquerdo - 10 */
+       
+        if (esquerdo <= 10) {
             esquerdo = 10;
-        }
+        } 
+        personagem.style.left = esquerdo + "px";
+        personagem.classList.add("esquerda");
     }
     /*awsd*/
-    else if(tecla == 83){
+    else if (tecla == 83) {
         topo += 10; /*topo = topo + 10 */
         personagem.style.top = topo + "px";
-        if(topo >= 400){
+        if (topo >= 400) {
             topo = 400;
         }
     }
-    else if(tecla == 87){
+    else if (tecla == 87) {
         topo -= 10; /*topo = topo - 10 */
-        personagem.style.top = topo + "px";
-        if(topo <= 10){
+        if (topo <= 10) {
             topo = 10;
         }
+        personagem.style.top = topo + "px";
+        personagem.classList.add("cima");
     }
-    else if(tecla == 68){
+    else if (tecla == 68) {
         esquerdo += 10; /*esquerdo = esquerdo + 10 */
-        personagem.style.left = esquerdo + "px";
-        if(esquerdo >= 598){
+        if (esquerdo >= 598) {
             esquerdo = 598;
         }
-    }
-    else if(tecla == 65){
-        esquerdo -= 10; /*esquerdo = esquerdo - 10 */
         personagem.style.left = esquerdo + "px";
-        if(esquerdo <= 10){
+        personagem.classList.add("direita");
+    }
+    else if (tecla == 65) {
+        esquerdo -= 10; /*esquerdo = esquerdo - 10 */
+        if (esquerdo <= 10) {
             esquerdo = 10;
         }
+        personagem.style.left = esquerdo + "px";
+        personagem.classList.add("esquerda");
     }
 }
